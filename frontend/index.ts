7a69ad4f4070e304485c6833e9b06818bff0b18c
harald1992 <harald_1992@hotmail.com>
2023-09-22 18:08:52 +0200

    nodemon for liverreload on html and ts files

--- frontend/index.ts
+++ frontend/index.ts
@@ -4,7 +4,6 @@ declare const document: any;
 
 (async () => {
   const userData = await fetchUser("harald1992");
-  // console.log(userData);
-  // console.log("log");
+
   document.querySelector("h1").innerHTML = JSON.stringify(userData);
 })();
